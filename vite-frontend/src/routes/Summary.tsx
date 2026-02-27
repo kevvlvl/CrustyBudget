abdfdddd8e9dcafdb8c0380f0fe28bbbe788dc89
kev <kev@notarealemail.com>
2026-02-26 23:55:59 -0500

    reformat

--- vite-frontend/src/routes/Summary.tsx
+++ vite-frontend/src/routes/Summary.tsx
@@ -1,6 +1,6 @@
 import { PieChart } from '@mantine/charts';
 import '@mantine/charts/styles.css'
-import {Box, Center, Grid, Loader, Paper, Table, type TableData} from "@mantine/core";
+import {Box, Center, Container, Grid, Loader, Paper, Table, type TableData} from "@mantine/core";
 import {useEffect, useMemo, useState} from "react";
 
 interface SummaryData {
@@ -20,9 +20,16 @@ interface ChartDataItem {
     color: string;
 }
 
-const COLOR_MAP: Record<string, string> = {
+const INCOME_COLOR_MAP: Record<string, string> = {
     Salary: 'orange.6',
     Investments: 'indigo.7',
+    Other: 'cyan.5',
+};
+
+const EXPENSE_COLOR_MAP: Record<string, string> = {
+    Utilities: 'red.3',
+    Electricity: 'blue.3',
+    Services: 'green.5',
     Other: 'cyan.5',
 };
 
@@ -30,6 +37,8 @@ export default function Summary() {
 
     const [incomeTableData, setIncomeTableData] = useState<SummaryDataItem[]>([]);
     const [incomeGraphData, setIncomeGraphData] = useState<ChartDataItem[]>([]);
+    const [expenseTableData, setExpenseTableData] = useState<SummaryDataItem[]>([]);
+    const [expenseGraphData, setExpenseGraphData] = useState<ChartDataItem[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
@@ -49,7 +58,7 @@ export default function Summary() {
                         name: rawData.items[i].category,
                         value: Number(rawData.items[i].amount),
                         // Fallback to gray if category isn't in our map
-                        color: COLOR_MAP[rawData.items[i].category] || 'gray.4',
+                        color: INCOME_COLOR_MAP[rawData.items[i].category] || 'gray.4',
                     });
                 }
 
@@ -67,9 +76,43 @@ export default function Summary() {
             .finally(() => {
                 setLoading(false);
             })
+
+        fetch('http://localhost:3000/api/budget/expense?frequency=Monthly')
+            .then(res => res.json())
+            .then(res => {
+
+                const rawData: SummaryData = res;
+
+                console.log('rawData = ', rawData);
+
+                const formattedChartData: ChartDataItem[] = [];
+
+                for(let i = 0; i < rawData.items.length; i++) {
+                    formattedChartData.push({
+                        name: rawData.items[i].category,
+                        value: Number(rawData.items[i].amount),
+                        // Fallback to gray if category isn't in our map
+                        color: EXPENSE_COLOR_MAP[rawData.items[i].category] || 'gray.4',
+                    });
+                }
+
+                console.log('formattedData = ', formattedChartData);
+                setExpenseTableData(rawData.items);
+                return formattedChartData;
+            })
+            .then(result => {
+                setExpenseGraphData(result);
+            })
+            .catch(error => {
+                console.error("Failed fetching: ", error);
+                setError(error);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
     }, []);
 
-    const tableData: TableData = useMemo(() => ({
+    const incomeTableDataProp: TableData = useMemo(() => ({
         head: ['Name', 'Category', 'Amount'],
         body: incomeTableData.map((item) => [
             item.name,
@@ -78,6 +121,15 @@ export default function Summary() {
         ]),
     }), [incomeTableData]);
 
+    const expenseTableDataProp: TableData = useMemo(() => ({
+        head: ['Name', 'Category', 'Amount'],
+        body: expenseTableData.map((item) => [
+            item.name,
+            item.category,
+            `$${Number(item.amount).toFixed(2)}`,
+        ]),
+    }), [expenseTableData]);
+
     if (loading)
         return <Center h={350}><Loader color="blue" /></Center>;
 
@@ -85,44 +137,70 @@ export default function Summary() {
         return <Center h={350}>Error: {error}</Center>;
 
     return(
+        <Container>
+            <Grid gutter="md" align="flex-start">
+                <Grid.Col span={{ base: 12, md: 6 }}>
 
-        <Grid gutter="md" align="flex-start">
-            <Grid.Col span={{ base: 12, md: 6 }}>
+                    <h2>Income</h2>
 
-                <h2>Income</h2>
+                    <Paper withBorder p="md" radius="md" h="100%">
+                        <Box h={280} w={300}>
+                            <PieChart
+                                data={incomeGraphData}
+                                labelsPosition={"outside"}
+                                labelsType={"value"}
+                                h={200}
+                                size={200}
+                                withLabels
+                                withLabelsLine
+                                withTooltip
+                            />
+                        </Box>
+                    </Paper>
 
-                <Paper withBorder p="md" radius="md" h="100%">
+                </Grid.Col>
 
-                <Box h={350} w={450}>
+                <Grid.Col span={{ base: 12, md: 6 }}>
 
-                    <PieChart
-                        data={incomeGraphData}
-                        labelsPosition={"outside"}
-                        labelsType={"value"}
-                        h={250}
-                        size={250}
-                        withLabels
-                        withLabelsLine
-                        withTooltip
-                    />
+                    <h2>Breakdown</h2>
 
-                </Box>
+                    <Paper withBorder p="md" radius="md" h="100%">
+                        <Table data={incomeTableDataProp} striped highlightOnHover verticalSpacing="sm" />
+                    </Paper>
 
-                </Paper>
+                </Grid.Col>
+            </Grid>
+            <Grid>
+                <Grid.Col span={{ base: 12, md: 6 }}>
 
-            </Grid.Col>
+                    <h2>Expenses</h2>
 
-            <Grid.Col span={{ base: 12, md: 6 }}>
+                    <Paper withBorder p="md" radius="md" h="100%">
+                        <Box h={200} w={200}>
+                            <PieChart
+                                data={expenseGraphData}
+                                labelsPosition={"outside"}
+                                labelsType={"value"}
+                                h={200}
+                                size={200}
+                                withLabels
+                                withLabelsLine
+                                withTooltip
+                            />
+                        </Box>
+                    </Paper>
 
-                <h2>Breakdown</h2>
+                </Grid.Col>
+                <Grid.Col span={{ base: 12, md: 6 }}>
 
-                <Paper withBorder p="md" radius="md" h="100%">
+                    <h2>Breakdown</h2>
 
-                <Table data={tableData} striped highlightOnHover verticalSpacing="sm" />
+                    <Paper withBorder p="md" radius="md" h="100%">
+                        <Table data={expenseTableDataProp} striped highlightOnHover verticalSpacing="sm" />
+                    </Paper>
 
-                </Paper>
-
-            </Grid.Col>
-        </Grid>
+                </Grid.Col>
+            </Grid>
+        </Container>
     );
 }
